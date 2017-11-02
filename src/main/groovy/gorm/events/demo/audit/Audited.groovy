eb7c02c645becb867d64ababb2909ee3cddb94f4
Jeff Scott Brown <brownj@objectcomputing.com>
2017-11-01 19:02:48 -0500

    Use static type checking

--- src/main/groovy/gorm/events/demo/audit/Audited.groovy
+++ src/main/groovy/gorm/events/demo/audit/Audited.groovy
@@ -1,5 +1,9 @@
 package gorm.events.demo.audit
 
+import groovy.transform.SelfType
+import org.grails.datastore.gorm.GormEntity
+
+@SelfType(GormEntity)
 trait Audited {
     Date dateCreated
     Date lastUpdated
--- src/main/groovy/gorm/events/demo/listener/AuditListener.groovy
+++ src/main/groovy/gorm/events/demo/listener/AuditListener.groovy
@@ -2,18 +2,21 @@ package gorm.events.demo.listener
 
 import gorm.events.demo.audit.Audited
 import grails.events.annotation.gorm.Listener
+import groovy.transform.CompileStatic
 import groovy.util.logging.Slf4j
 import org.grails.datastore.mapping.engine.event.ValidationEvent
 
 @Slf4j
+@CompileStatic
 class AuditListener {
 
     @Listener(Audited)
     void validateEvent(ValidationEvent event) {
-        log.debug "Validating [${event.entityObject}]"
-        if(!event.entityObject.attached) {
+        Audited entity = event.entityObject as Audited
+        if(!entity.isAttached()) {
+            log.debug "Setting createdBy for instance [${entity}]"
             // Could get user info from security context, etc...
-            event.entityObject.createdBy = 'some-user-id'
+            entity.createdBy = 'some-user-id'
         }
     }
 }
